c3d645871bef517b8d416287dc1ee411989a2af6
Amrit Sehgal <AmritGithub@gmail.com>
2022-03-04 22:44:14 -0600

    Rewrote setSymbolRate to use dynamic symbol rate

--- arduino_unit_tests/cc1120_setup.cpp
+++ arduino_unit_tests/cc1120_setup.cpp
@@ -2,6 +2,20 @@
 
 #include "cc1120_spi.h"
 #include "cc1120_regs.h"
+#include <math.h>
+#include <stdbool.h>
+
+/**
+ * @brief Gets the nth to kth bits of an unsigned integer.
+ * 
+ * @param number - The number to get bits from.
+ * @param start - The lowest bit to get (0-indexed).
+ * @param end - the highest bit to get (0-indexed).
+ * @return The integer equal to the slice of bits taken.
+ */
+int NtoKBits(int number, int start, int end) {
+    return (number >> start) & ((1 << (end + 1)) - 1);
+}
 
 /**
  * @brief Sets modulation format to 2-GFSK.
@@ -15,12 +29,26 @@ void setGSFK() {
 }
 
 /**
- * @brief Sets data rate to 9600 symbols per second.
+ * @brief Sets symbol rate.
  * 
+ * @param symbolRate - The symbol rate to set to, in symbols per second.
+ * @return true - If the operation was successful
+ * @return false - If the operation was not successful.
  */
-void setDataRate() {
-    // STRATE_E = 0, STRATE_M = 0x1421F
-    arduinoWriteSPI(SYMBOL_RATE2, 0b00000001);
-    arduinoWriteSPI(SYMBOL_RATE1, 0b01000010);
-    arduinoWriteSPI(SYMBOL_RATE0, 0b00011111);
+bool setSymbolRate(int symbolRate) {
+    long int FXOSC = 32000000;
+
+    int STRATE_E = 0;
+    long int STRATE_M = (pow(2, 38) * ((double)symbolRate / 1000)) / FXOSC;
+
+    if (STRATE_M > pow(2, 20) - 1) {
+        // Larger than largest 20 bit unsigned integer
+        return false;
+    }
+
+    bool succeeded = true;
+    succeeded = succeeded & arduinoWriteSPI(SYMBOL_RATE2, STRATE_M << 4 || NtoKBits(STRATE_M, 16, 19));
+    succeeded = succeeded & arduinoWriteSPI(SYMBOL_RATE1, NtoKBits(STRATE_M, 8, 15));
+    succeeded = succeeded & arduinoWriteSPI(SYMBOL_RATE0, NtoKBits(STRATE_M, 0, 7));
+    return succeeded;
 }
