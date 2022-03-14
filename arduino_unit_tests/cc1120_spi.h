f38b6c2671eef04af4ad06b5761030e06e8e80b2
Aryan Kalia <kalia.aryan2013@gmail.com>
2022-03-14 09:31:23 -0400

    add comments to .h

--- arduino_unit_tests/cc1120_spi.h
+++ arduino_unit_tests/cc1120_spi.h
@@ -18,23 +18,186 @@ union cc_st {
   uint8_t v;
 };
 
+/**
+ * @brief Reads a register from the CC1120.
+ * 
+ * @param addr - The address of the register to read.
+ * @param data - Pointer to the variable to store the read data.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoReadSPI(uint8_t addr, uint8_t *data);
+
+/**
+ * @brief - Reads consecutive registers from the CC1120 in burst access mode.
+ * 
+ * @param addr - The address of the first register to read.
+ * @param data - The array to store the read data.
+ * @param len - The number of registers to read.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstReadSPI(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief Reads an extended address space register from the CC1120.
+ * 
+ * @param addr - The address of the register to read, starting at 0x00.
+ * @param data - Pointer to the variable to store the read data.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoReadExtAddrSPI(uint8_t addr, uint8_t *data);
+
+/**
+ * @brief - Reads consecutive extended address space registers from the CC1120 in burst mode.
+ * 
+ * @param addr - The address of the first register to read.
+ * @param data - The array to store the read data.
+ * @param len - The number of registers to read.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstReadExtAddrSPI(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief Writes to a register on the CC1120.
+ * 
+ * @param addr - The address of the register to write to.
+ * @param data - The data to write to the register.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoWriteSPI(uint8_t addr, uint8_t data);
+
+/**
+ * @brief - Writes consecutive registers on the CC1120 in burst mode.
+ * 
+ * @param addr - The address of the first register to write to.
+ * @param data - The array of data to write to the registers.
+ * @param len - The number of registers to write.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstWriteSPI(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief Writes to an extended address space register on the CC1120.
+ * 
+ * @param addr - The address of the register to write to, starting at 0x00.
+ * @param data - The data to write to the register.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoWriteExtAddrSPI(uint8_t addr, uint8_t data);
+
+/**
+ * @brief - Writes consecutive extended address space registers on the CC1120 in burst mode.
+ * 
+ * @param addr - The address of the first register to write to.
+ * @param data - The array of data to write to the registers.
+ * @param len - The number of registers to write.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstWriteExtAddrSPI(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief Calls a strobe command on the CC1120.
+ * 
+ * @param addr - The address of the strobe command.
+ * @return true - If the strobe command was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoStrobeSPI(uint8_t addr);
+
+/**
+ * @brief Reads from FIFO RX queue on the CC1120.
+ * 
+ * @param data - Pointer to the variable to store the read data.
+ * @return true - If the read was successful.
+ * @return false - If the status byte is invalid.
+ */
 bool arduinoReadFIFO(uint8_t *data);
+
+/**
+ * @brief - Reads consecutive registers from the FIFO memory in burst mode.
+ * 
+ * @param data - The array to store the read data.
+ * @param len - The number of registers to read.
+ * @return true - If the read was successful.
+ * @return false - If the status byte is invalid.
+ */
 bool arduinoBurstReadFIFO(uint8_t data[], uint8_t len);
+
+/**
+ * @brief Writes to FIFO TX queue on the CC1120.
+ * 
+ * @param data - The data to write to the FIFO.
+ * @return true - If the write was successful.
+ * @return false - If the status byte is invalid.
+ */
 bool arduinoWriteFIFO(uint8_t data);
+
+/**
+ * @brief - Writes consecutive registers to the FIFO memory in burst mode.
+ * 
+ * @param data - The array of data to write to the FIFO.
+ * @param len - The number of registers to write.
+ * @return true - If the write was successful.
+ * @return false - If the status byte is invalid.
+ */
 bool arduinoBurstWriteFIFO(uint8_t data[], uint8_t len);
+
+/**
+ * @brief Reads directly from the FIFO on the CC1120.
+ * 
+ * @param addr - The address of the register to read. Range 0x00 - 0xFF.
+ * @param data - Pointer to the variable to store the read data.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoReadFIFODirect(uint8_t addr, uint8_t *data);
+
+/**
+ * @brief - Reads consecutive registers directly from the FIFO on the CC1120 in burst mode.
+ * 
+ * @param addr - The address of the first register to read. Range 0x00 - 0xFF.
+ * @param data - The array to store the read data.
+ * @param len - The number of registers to read.
+ * @return true - If the read was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstReadFIFODirect(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief Writes directly to the FIFO on the CC1120.
+ * 
+ * @param addr - The address of the register to write to. Range 0x00 - 0xFF.
+ * @param data - The data to write to the register.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoWriteFIFODirect(uint8_t addr, uint8_t data);
+
+/**
+ * @brief - Writes consecutive registers directly to the FIFO on the CC1120 in burst mode.
+ * 
+ * @param addr - The address of the first register to write to. Range 0x00 - 0xFF.
+ * @param data - The array of data to write to the registers.
+ * @param len - The number of registers to write.
+ * @return true - If the write was successful.
+ * @return false - If the register is not valid, or the status byte is invalid.
+ */
 bool arduinoBurstWriteFIFODirect(uint8_t addr, uint8_t data[], uint8_t len);
+
+/**
+ * @brief - Reads the status register on the CC1120 and consecutively sends a byte over SPI.
+ * 
+ * @param data - The data to send to the status register.
+ * @return true - If the status byte is valid.
+ * @return false - If the status byte is invalid.
+ */
 bool sendByteReceiveStatus(uint8_t data);
 
 #endif /* SPI_CC1120_H */
--- arduino_unit_tests/cc1120_unit_tests.h
+++ arduino_unit_tests/cc1120_unit_tests.h
@@ -4,10 +4,55 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * @brief Set the default values for CC1120 registers.
+ * 
+ */
 void setRegisterDefaults();
+
+/**
+ * @brief Unit test for SPI read function.
+ * Reads through all registers up to the extended register space,
+ * and compares values to default values.
+ * Burst reads all the values and compares them to the default values.
+ * Burst reads FREQ registers in extended address space and compares them to defaults.
+ * 
+ * @return true - If all registers are read correctly and have the right value.
+ * @return false - If any register does not have the expected value,
+ *                 or status byte is invalid.
+ */
 bool checkSPIRead();
+
+/**
+ * @brief Unit test for SPI write function.
+ * Writes to frequency offset register and extended address space RNDGEN register.
+ * Reads the registers to see if the write was successful.
+ * Burst writes to the sync word registers, then burst reads to see if the writes were successful.
+ * Burst writes to the FREQ registers, then burst reads to see if the writes were successful.
+ * 
+ * @return true - If both writes are successful.
+ * @return false - If any register does not have the expected value,
+ *                 or status byte is invalid.
+ */
 bool checkSPIWrite();
+
+/**
+ * @brief Unit test for SPI strobe functionality.
+ * Runs the reset strobe and checks the MARCSTATE register.
+ * 
+ * @return true - If MARCSTATE is 0x41 after reset.
+ * @return false - If MARCSTATE is not 0x41 after reset, or status byte is invalid.
+ */
 bool checkStrobe();
+
+/**
+ * @brief - Unit test for the CC1120's FIFO read and write functionality.
+ * Writes to the FIFO and then does a direct read to see if the write was successful.
+ * Wries directly to the FIFO and then reads the FIFO to see if the write was successful.
+ * 
+ * @return true - If the FIFO read and write tests pass.
+ * @return false - If the FIFO read and write tests fail.
+ */
 bool checkFIFOReadWrite();
 
 #endif /* UNIT_TESTS_CC1120_H */
